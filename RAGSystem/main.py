8a955e71d6c1a3fe6285d6d0ae690d3d3d55657b
Timik232 <komolov.timurka@mail.ru>
2024-11-06 23:57:10 +0300

    Fix many and many errors (it's never over)

--- RAGSystem/main.py
+++ RAGSystem/main.py
@@ -15,44 +15,20 @@ def simple_rag_init(
     url = "http://localhost:1234/v1/chat/completions"
     tokenizer = AutoTokenizer.from_pretrained(embedding_model_name)
     model = AutoModel.from_pretrained(embedding_model_name)
+    model = model
     if logging:
         print("Embedding model was loaded")
     llm = CustomAPILLM(api_url=url, model_name=llm_model_name)
     if logging:
         print("LLM model was created")
-    rag = RAG(tokenizer, model, war_and_peace, "war_and_peace", llm)
+    rag = RAG(
+        tokenizer, model, war_and_peace, "war_and_peace", llm, embedding_model_name
+    )
     if logging:
         print("RAG system was created")
     return rag
 
 
 if __name__ == "__main__":
-    rag = simple_rag_init()
+    rag = simple_rag_init("../war_and_peace_small.txt")
     print(rag("Кто главный персонаж книги война и мир?"))
-
-"""
-#init Vector DB
-embeddings = OpenAIEmbeddings()
-
-doc_store = Qdrant.from_documents(
-    war_and_peace,
-    embeddings,
-    location=":memory:",
-    collection_name="docs",
-)
-
-llm = OpenAI()
-# ask questions
-
-while True:
-    question = input('Ваш вопрос: ')
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=doc_store.as_retriever(),
-        return_source_documents=False,
-    )
-
-    result = qa(question)
-    print(f"Answer: {result}")
-"""
